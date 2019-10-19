72da6fff3ef6a0be40f5dda6675ac4facae7c13c
Ricardo Bravo <bbravo@belatrixsf.com>
2019-10-19 18:40:13 -0500

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Ricardo Bravo Acuña on 10/19/19.
+//  Copyright © 2019 Ricardo Bravo Acuña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
